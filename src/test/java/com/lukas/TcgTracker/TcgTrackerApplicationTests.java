4ee4f1571bef554281453769ca93186a322b7325
Lukas <Trampus749@gmail.com>
2025-05-18 14:33:53 +0200

    init commoit

---
+++ src/test/java/com/lukas/TcgTracker/TcgTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukas.TcgTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcgTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
